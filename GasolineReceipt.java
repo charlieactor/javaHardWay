76fc0c7af94947e46d0f5deeca28b92589232a54
Charlie Actor <Charlie@Charless-MacBook-Pro.local>
2017-05-23 12:07:27 -0600

    add exercise 2

---
+++ GasolineReceipt.java
@@ -0,0 +1,16 @@
+public class GasolineReceipt {
+	public static void main( String[] args) {
+		System.out.println( "+----------------------+" );
+		System.out.println( "|                      |" );
+		System.out.println( "|      CORNER STORE    |" );
+		System.out.println( "|                      |" );
+		System.out.println( "| 2017-05-23 11:42AM   |" );
+		System.out.println( "|                      |" );
+		System.out.println( "|Gallons         10.870|" );
+		System.out.println( "|Price/Gallon   $2.089 |" );
+		System.out.println( "|                      |" );
+		System.out.println( "| Fuel Total $ 22.71   |" );
+		System.out.println( "|                      |" );
+		System.out.println( "+----------------------+" );
+		}
+	}
