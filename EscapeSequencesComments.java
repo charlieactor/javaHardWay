cc7419f58d5fe669a269ea5c9d046b30c68c4fa0
Charlie Actor <Charlie@Charless-MacBook-Pro.local>
2017-05-23 13:50:34 -0600

    add exercise 4

---
+++ EscapeSequencesComments.java
@@ -0,0 +1,19 @@
+public class EscapeSequencesComments {
+	public static void main( String[] args) {
+		// This exerice demonstrates escape sequences and comments (like these)!
+		System.out.print( "Learn\tJava\n\tthe\nHard\tWay\n\n" );
+		System.out.print( "\tLearn Jave the \"Hard\" Way!\n" );
+		// System.out.frimp( "Learn Java the hard way" );
+		System.out.print( "Hello\n" ); //This line prints hello.
+		System.out.print( "Jello\by\n" ); //This line prints Jelly.
+		/* The quick brown fox jumped over a lazy dog.
+		Quick wafting zephyrs vex bold jim */
+		System.out.println( "Hard to believe, eh?");
+		System.out.println( "Surprised? /* abcde */ Or what did you expect?" );
+		System.out.println( "\\ // -=- \\ //" );
+		System.out.println( "\\\\ \\\\\\ \\\\\\\\" ); //it takes 2 to make 1.
+		System.out.print( "I hope you understand \"escape sequences\" now. \n" );
+		//and comments.
+		}
+	}
+	
